ca0f03e4395da07a3f475ea1e6ce44f8975656ea
Ben Johnson <ben@bjmfactory.com>
2015-04-29 19:32:25 -0500

    adds incomplete count

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -10,9 +10,15 @@ if (Meteor.isClient) {
         return Tasks.find({}, {sort: {createdAt: -1}});
       }
     },
+
     hideCompleted: function(){
       return Session.get("hideCompleted");
+    },
+
+    incompleteCount: function(){
+      return Tasks.find({checked: {$ne: true}}).count();
     }
+
   });
 
   Template.body.events({
